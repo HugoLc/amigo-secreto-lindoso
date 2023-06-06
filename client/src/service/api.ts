ed85837c23917e3778dbbbc329a63995cafe7718
Hugo Lindoso <hugo.correia@agenciam3.com>
2023-06-06 19:52:24 -0300

    fix(client): remove utilização daporta na url base do axios

--- client/src/service/api.ts
+++ client/src/service/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:6969/api",
+  // baseURL: "http://localhost:6969/api",
+  baseURL: "http://localhost/api",
 });
 
 export default api;

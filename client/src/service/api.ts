91162bca5df26aeee7431fd1d1557b31b855838b
Hugo Lindoso <hugo.correia@agenciam3.com>
2023-06-09 16:01:57 -0300

    fix: axios base url

--- client/src/service/api.ts
+++ client/src/service/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:6969/api",
-  baseURL: "https://localhost/api",
+  baseURL: "/api",
 });
 
 export default api;
